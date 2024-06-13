dbafe41eb233761b8be8be40ea252e967f5173a2
AlexXD465 <100385240+AlexXD465@users.noreply.github.com>
2024-06-13 07:57:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
+#from flask import Flask, render_template
+#from flask_socketio import SocketIO, send
+
+#app = Flask(__name__)
+#app.config['SECRET_KEY'] = 'secret'
+#socketio = SocketIO(app)
+
+#@app.route('/')
+#def index():
+ #   return render_template('index.html')
+
+#@socketio.on('message')
+#def handleMessage(msg):
+ #   print("Message:" + msg)
+  #  send(msg, broadcast = True)
+
+#if __name__ == '__main__':
+ #   socketio.run(app)
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @socketio.on('message')
 def handleMessage(msg):
-    print("Message:" + msg)
-    send(msg, broadcast = True)
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
 
+
+# Routes to Render Something
+@app.route('/')
+def home():
+  return render_template("home.html")
+
+
+@app.route('/chat')
+def chat():
+  return render_template("chat.html")
+
+
+@app.route('/accounts')
+def about():
+  return render_template("accounts.html")
+
+
+# Make sure this we are executing this file
 if __name__ == '__main__':
-    socketio.run(app)
+  app.run(host='0.0.0.0', port=8080)
+  app.run(debug=True)
+  app.run(app)
